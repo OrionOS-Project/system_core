OrionOS-Project/system_core

commit d8e680dcd4904ac88bd1237fefdd748e72f87eb8

libprocessgroup: Mount cpuset v1 with cpuset_v2_mode
The Android kernel used to carry a patch to restore cpuset masks upon
CPU hotplug onlining, however this was reverted before 6.1. [1] So
devices running kernels >= 6.1 currently suffer from a problem where
cpusets do not include CPUs that they should when CPU hotplugging
occurs.

Fix this by switching from the cpuset filesystem type to cgroup, and by
mounting with the cpuset_v2_mode option which fixes the behavior. It is
possible to use cpuset_v2_mode with the cpuset filesystem type with
kernels that have [2], but deploying this kernel update to already
shipped devices would require waiting 1+ year before we could make this
platform update (and retain the current cpuset filesystem type).

Note that the release_agent option is now intentionally omitted, as it
is unused on Android.

[1] https://r.android.com/2251493
[2] https://lore.kernel.org/all/20250416211752.945849-1-tjmercier@google.com/

Bug: 409240872
(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:cb48d31d1289010cc997ab4a9db1f7b034593b4e)
Merged-In: I8e39c2831db06d65b6cbbe82f3199272fa85d637
Change-Id: I8e39c2831db06d65b6cbbe82f3199272fa85d637

libprocessgroup/setup/cgroup_map_write.cpp
@@ -189,22 +189,19 @@ static bool MountV1CgroupController(const CgroupDescriptor& descriptor) {
         return false;
     }
 
-    // Unfortunately historically cpuset controller was mounted using a mount command
-    // different from all other controllers. This results in controller attributes not
-    // to be prepended with controller name. For example this way instead of
-    // /dev/cpuset/cpuset.cpus the attribute becomes /dev/cpuset/cpus which is what
-    // the system currently expects.
-    int res;
+    std::string options = controller->name();
+
     if (!strcmp(controller->name(), "cpuset")) {
-        // mount cpuset none /dev/cpuset nodev noexec nosuid
-        res = mount("none", controller->path(), controller->name(),
-                    MS_NODEV | MS_NOEXEC | MS_NOSUID, nullptr);
-    } else {
-        // mount cgroup none <path> nodev noexec nosuid <controller>
-        res = mount("none", controller->path(), "cgroup", MS_NODEV | MS_NOEXEC | MS_NOSUID,
-                    controller->name());
+        // Android depends on the noprefix option for cpuset so that cgroupfs files are not prefixed
+        // with the controller name. For example /dev/cpuset/cpus instead of
+        //                                       /dev/cpuset/cpuset.cpus.
+        // cpuset_v2_mode is required to restore the original cpu mask after a cpu is offlined, and
+        // then onlined in cgroup v1.
+        options += ",noprefix,cpuset_v2_mode";
     }
-    if (res != 0) {
+
+    if (mount("none", controller->path(), "cgroup", MS_NODEV | MS_NOEXEC | MS_NOSUID,
+              options.c_str())) {
         if (IsOptionalController(controller)) {
             PLOG(INFO) << "Failed to mount optional controller " << controller->name();
             return true;

commit 21128b1b60db6dacc4abab51a88c61815f2b8ea2

[BUGFIX] fix pre-5.x cgroup mounting
d8e680dcd4904ac88bd1237fefdd748e72f87eb8 caused cgroup errors on pre-5.x devices

xiaomi beryllium logs
10-14 03:25:30.651  4782  5121 W libprocessgroup: SetCgroup::ExecuteForTask: failed to open /dev/cpuset/foreground/tasks: No such file or directory
10-14 03:25:01.545  1925  2022 W libprocessgroup: SetCgroup::ExecuteForProcess: failed to open /dev/cpuset/top-app/cgroup.procs: No such file or directory
10-14 03:25:02.337  1925  2022 W libprocessgroup: SetCgroup::ExecuteForProcess: failed to open /dev/cpuset/background/cgroup.procs: No such file or directory
10-14 03:25:21.074  1925  2011 E libprocessgroup: Failed to write '4547' to /sys/fs/cgroup/apps/uid_99000/pid_4547/cgroup.procs: Permission denied
10-14 03:25:21.074  1925  2011 E AndroidRuntime: *** FATAL EXCEPTION IN SYSTEM PROCESS: ActivityManager:procStart
10-14 03:25:21.074  1925  2011 E AndroidRuntime: java.lang.AssertionError: Unable to create process group for com.android.webview:sandboxed_process0:org.chromium.content.app.SandboxedProcessService0:0 (4547)
10-14 03:25:21.074  1925  2011 E AndroidRuntime: 	at com.android.server.am.ProcessList.startProcess(ProcessList.java:2658)
10-14 03:25:21.074  1925  2011 E AndroidRuntime: 	at com.android.server.am.ProcessList.lambda$handleProcessStart$1(ProcessList.java:2257)
10-14 03:25:21.074  1925  2011 E AndroidRuntime: 	at com.android.server.am.ProcessList.$r8$lambda$3aT8AjDesFyouy4iqaLN2hUmayA(ProcessList.java:0)
10-14 03:25:21.074  1925  2011 E AndroidRuntime: 	at com.android.server.am.ProcessList$$ExternalSyntheticLambda1.run(R8$$SyntheticClass:0)
10-14 03:25:21.074  1925  2011 E AndroidRuntime: 	at com.android.server.am.ProcessList.handleProcessStart(ProcessList.java:2284)
10-14 03:25:21.074  1925  2011 E AndroidRuntime: 	at com.android.server.am.ProcessList.lambda$startProcessLocked$0(ProcessList.java:2223)

libprocessgroup/setup/cgroup_map_write.cpp
@@ -25,6 +25,7 @@
 #include <sys/mount.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/utsname.h>
 #include <unistd.h>
 
 #include <android-base/logging.h>
@@ -180,6 +181,18 @@ static bool ActivateV2CgroupController(const CgroupDescriptor& descriptor) {
     return ::ActivateControllers(controller->path(), {{controller->name(), descriptor}});
 }
 
+static bool IsCpusetV2ModeEnabled() {
+    struct utsname uts;
+    unsigned int major;
+
+    if (uname(&uts) != 0 || sscanf(uts.release, "%u", &major) != 1) {
+        LOG(ERROR) << "Could not parse the kernel version from uname";
+        return false;
+    }
+
+    return (major >= 5);
+}
+
 static bool MountV1CgroupController(const CgroupDescriptor& descriptor) {
     const CgroupController* controller = descriptor.controller();
 
@@ -188,26 +201,53 @@ static bool MountV1CgroupController(const CgroupDescriptor& descriptor) {
         LOG(ERROR) << "Failed to create directory for " << controller->name() << " cgroup";
         return false;
     }
+    
+    if (IsCpusetV2ModeEnabled()) {
+        std::string options = controller->name();
 
-    std::string options = controller->name();
-
-    if (!strcmp(controller->name(), "cpuset")) {
-        // Android depends on the noprefix option for cpuset so that cgroupfs files are not prefixed
-        // with the controller name. For example /dev/cpuset/cpus instead of
-        //                                       /dev/cpuset/cpuset.cpus.
-        // cpuset_v2_mode is required to restore the original cpu mask after a cpu is offlined, and
-        // then onlined in cgroup v1.
-        options += ",noprefix,cpuset_v2_mode";
-    }
-
-    if (mount("none", controller->path(), "cgroup", MS_NODEV | MS_NOEXEC | MS_NOSUID,
-              options.c_str())) {
-        if (IsOptionalController(controller)) {
-            PLOG(INFO) << "Failed to mount optional controller " << controller->name();
-            return true;
+        if (!strcmp(controller->name(), "cpuset")) {
+            // Android depends on the noprefix option for cpuset so that cgroupfs files are not prefixed
+            // with the controller name. For example /dev/cpuset/cpus instead of
+            //                                       /dev/cpuset/cpuset.cpus.
+            // cpuset_v2_mode is required to restore the original cpu mask after a cpu is offlined, and
+            // then onlined in cgroup v1.
+            options += ",noprefix,cpuset_v2_mode";
         }
-        PLOG(ERROR) << "Failed to mount controller " << controller->name();
-        return false;
+
+        if (mount("none", controller->path(), "cgroup", MS_NODEV | MS_NOEXEC | MS_NOSUID,
+                  options.c_str())) {
+            if (IsOptionalController(controller)) {
+                PLOG(INFO) << "Failed to mount optional controller " << controller->name();
+                return true;
+            }
+            PLOG(ERROR) << "Failed to mount controller " << controller->name();
+            return false;
+        }
+    } else {
+        // Unfortunately historically cpuset controller was mounted using a mount command
+        // different from all other controllers. This results in controller attributes not
+        // to be prepended with controller name. For example this way instead of
+        // /dev/cpuset/cpuset.cpus the attribute becomes /dev/cpuset/cpus which is what
+        // the system currently expects.
+        int res;
+        if (!strcmp(controller->name(), "cpuset")) {
+            // mount cpuset none /dev/cpuset nodev noexec nosuid
+            res = mount("none", controller->path(), controller->name(),
+                        MS_NODEV | MS_NOEXEC | MS_NOSUID, nullptr);
+        } else {
+            // mount cgroup none <path> nodev noexec nosuid <controller>
+            res = mount("none", controller->path(), "cgroup", MS_NODEV | MS_NOEXEC | MS_NOSUID,
+                        controller->name());
+        }
+        
+        if (res != 0) {
+            if (IsOptionalController(controller)) {
+                PLOG(INFO) << "Failed to mount optional controller " << controller->name();
+                return true;
+            }
+            PLOG(ERROR) << "Failed to mount controller " << controller->name();
+            return false;
+        }
     }
     return true;
 }